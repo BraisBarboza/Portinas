53cad8a23812f028cba4776bfcb3691ba1a03b1c
julian.bfacal <julian.bfacal@udc.es>
2021-11-01 17:19:54 +0100

    "Creación da actividade principal con progress bar e botons"

--- app/src/main/java/com/example/portinas/MainActivity.java
+++ app/src/main/java/com/example/portinas/MainActivity.java
@@ -1,15 +1,63 @@
 package com.example.portinas;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int progr_num = 0;
+    private int progr_per = 0;
+    private int aforo_total = 0;
+    @BindView(R.id.progress_bar_txt)
+    TextView progress_tv;
+    @BindView(R.id.button_increase)
+    Button but_increment;
+    @BindView(R.id.button_decrease)
+    Button but_decrement;
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.Theme_Portinas);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String aforo_num = bundle.getString(EnterAforoActivity.KEY_TEXT);
+            aforo_total = Integer.parseInt(aforo_num);
+            progress_tv.setText(progr_num + "/" + aforo_num);
+            progressBar.setMax(aforo_total);
+        }
+
+    }
+
+    @OnClick(R.id.button_increase)
+    public void submitButtonIncrease(){
+        if (progr_num < aforo_total){
+            progr_num += 1;
+            updateProgressBar();
+        }
+    }
+
+    @OnClick(R.id.button_decrease)
+    public void submitButtonDecrease(){
+        if (progr_num > 0){
+            progr_num -= 1;
+            updateProgressBar();
+        }
+    }
+
+    private void updateProgressBar(){
+        progressBar.setProgress(progr_num);
+        progress_tv.setText(progr_num + "/" + aforo_total);
     }
 }
