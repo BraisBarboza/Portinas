8e7e408d39d4eef2d67c2b73a791f4de44257307
julian.bfacal <julian.bfacal@udc.es>
2021-11-02 01:11:41 +0100

    "Creado Navegation Drawer, sin funcionalidades activadas"

--- app/src/main/java/com/example/portinas/MainActivity.java
+++ app/src/main/java/com/example/portinas/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.portinas;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActionBarContextView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private int progr_num = 0;
     private int progr_per = 0;
     private int aforo_total = 0;
+    private ActionBarDrawerToggle toggle;
     @BindView(R.id.progress_bar_txt)
     TextView progress_tv;
     @BindView(R.id.button_increase)
@@ -24,11 +32,16 @@ public class MainActivity extends AppCompatActivity {
     Button but_decrement;
     @BindView(R.id.progress_bar)
     ProgressBar progressBar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(toolbar);
         ButterKnife.bind(this);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
@@ -36,10 +49,26 @@ public class MainActivity extends AppCompatActivity {
             aforo_total = Integer.parseInt(aforo_num);
             progress_tv.setText(progr_num + "/" + aforo_num);
             progressBar.setMax(aforo_total);
+
         }
+         toggle =  new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+        toggle.syncState();
 
     }
 
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
     @OnClick(R.id.button_increase)
     public void submitButtonIncrease(){
         if (progr_num < aforo_total){
